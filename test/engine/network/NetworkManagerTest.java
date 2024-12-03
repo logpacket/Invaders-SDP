bfc86ed59e2f4590aea0a95d5b52e174dffecb46
rooftop <jmkjmk6666@gmail.com>
2024-12-03 17:36:57 +0900

    feat: Add NetworkManager ping latency test (WIP)

--- test/engine/network/NetworkManagerTest.java
+++ test/engine/network/NetworkManagerTest.java
@@ -1,7 +1,18 @@
 package engine.network;
 
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
+
 class NetworkManagerTest {
+    private NetworkManager networkManager;
+
+    @BeforeEach
+    void setUp() {
+        networkManager = NetworkManager.getInstance();
+    }
 
     @org.junit.jupiter.api.Test
     void getInstance() {
@@ -21,5 +32,18 @@ class NetworkManagerTest {
 
     @org.junit.jupiter.api.Test
     void close() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pingTest() {
+        long expectedLatency = 100;
+
+        long sendTime = System.currentTimeMillis() - expectedLatency;
+        Ping testPing = new Ping(sendTime);
+        Event pingEvent = new Event("ping", testPing, Status.OK, System.currentTimeMillis());
+
+        // run dispatch
+        //networkManager.dispatch(pingEvent);
+
     }
 }
