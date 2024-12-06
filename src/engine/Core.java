6713aa58faaf5bbcd64ba2f6b052605493802a92
rooftop <jmkjmk6666@gmail.com>
2024-12-07 03:10:47 +0900

    feat: Add matchmaking screen (WIP)

--- src/engine/Core.java
+++ src/engine/Core.java
@@ -159,6 +159,11 @@ public final class Core {
 					menu = frame.setScreen(currentScreen);
 					break;
 
+				case MATCHMAKING:
+					currentScreen = new MatchMakingScreen(width, height, FPS);
+					menu = frame.setScreen(currentScreen);
+					break;
+
                 default:
                     break;
             }
